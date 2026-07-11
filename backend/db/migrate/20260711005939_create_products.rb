da7200490c6567ff9d5ca07592a57bc83d99ce0f
Kelly Maass <kmaass77627@gmail.com>
2026-07-10 20:04:53 -0500

    added migrations

---
+++ backend/db/migrate/20260711005939_create_products.rb
@@ -0,0 +1,14 @@
+class CreateProducts < ActiveRecord::Migration[8.1]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.text :description
+      t.decimal :price
+      t.string :image_url
+      t.string :category
+      t.integer :inventory_count
+
+      t.timestamps
+    end
+  end
+end
